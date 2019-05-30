8e71336fa239e7404a28811630c8388fb2c45896
SharlonM <sharlon12@hotmail.com>
2019-05-29 22:53:07 -0300

    First commit

--- app/src/main/java/com/sharlon/projetoblack/ActivityInfo.java
+++ app/src/main/java/com/sharlon/projetoblack/ActivityInfo.java
@@ -9,5 +9,6 @@ public class ActivityInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        System.out.println("teste");
     }
 }
