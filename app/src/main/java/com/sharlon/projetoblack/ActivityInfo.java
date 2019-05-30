39ca9839301cddd13f971ee2ec379368f4f10431
SharlonM <sharlon12@hotmail.com>
2019-05-29 23:00:22 -0300

    Second commit

--- app/src/main/java/com/sharlon/projetoblack/ActivityInfo.java
+++ app/src/main/java/com/sharlon/projetoblack/ActivityInfo.java
@@ -10,6 +10,6 @@ public class ActivityInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         System.out.println("teste");
-        System.out.println("Teste second commit");
+        System.out.println("Teste second commit n sei pq");
     }
 }
